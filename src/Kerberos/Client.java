1a947f0797feb4390f0429f70c96a41befb2a30d
kaib <kai.bielenberg@haw-hamburg.de>
2013-01-08 08:53:26 +0100

    prüfung berichtigt

--- src/Kerberos/Client.java
+++ src/Kerberos/Client.java
@@ -31,50 +31,64 @@ public class Client extends Object {
 	public boolean login(String userName, char[] password) {
 		String tgsServerName = "myTGS";
 		currentUser = userName;
-		//Anmeldung beim KDC, TGS Ticket holen
-		TicketResponse ticketResponse = myKDC.requestTGSTicket(userName, tgsServerName, generateNonce());
-		//TGS Response Entschlüsseln und auswerten
-		if (ticketResponse != null) {//TGS beim KDC Server bekannt
+		// Anmeldung beim KDC, TGS Ticket holen
+		long n1 = generateNonce();
+		TicketResponse ticketResponse = myKDC.requestTGSTicket(userName,
+				tgsServerName, n1);
+		// TGS Response Entschlüsseln und auswerten
+		if (ticketResponse != null) {// TGS beim KDC Server bekannt
+			
 			long pw = generateSimpleKeyForPassword(password);
 			if (ticketResponse.decrypt(pw)) {
-				//TGS Session Key und Ticket speichern
+				// TGS Session Key und Ticket speichern
+				if(n1 == ticketResponse.getNonce()) {
 				tgsSessionKey = ticketResponse.getSessionKey();
 				tgsTicket = ticketResponse.getResponseTicket();
 				tgsTicket.print();
+				} else {
+					System.out.println("Ticket not verified");
+				}
+				
 			}
-			//PAssword aus Hauptspeicher löschen
+			// PAssword aus Hauptspeicher löschen
 			Arrays.fill(password, '0');
 		}
-		
+
 		return (tgsTicket != null);
 	}
 
 	public boolean showFile(String serverName, String filePath) {
 		boolean status = false;
-		//Login Prüfen TGS Ticket vorhanden?
-		if (tgsTicket != null) {//Ohne TGS Ticket geht nichts	
-			//Serverticket vorhanden?
-			if (serverTicket == null) {//Kein Serverticket da, neues Anfordern
+		// Login Prüfen TGS Ticket vorhanden?
+		if (tgsTicket != null) {// Ohne TGS Ticket geht nichts
+			// Serverticket vorhanden?
+			if (serverTicket == null) {// Kein Serverticket da, neues Anfordern
 				long currentTime = (new Date()).getTime();
 				Auth tgsAuth = new Auth(currentUser, currentTime);
 				tgsAuth.encrypt(tgsSessionKey);
-				TicketResponse ticketResponse = myKDC.requestServerTicket(tgsTicket, tgsAuth, serverName, generateNonce());
+				long n2 = generateNonce();
+				TicketResponse ticketResponse = myKDC.requestServerTicket(
+						tgsTicket, tgsAuth, serverName, n2);
 				ticketResponse.print();
-				if(ticketResponse.decrypt(tgsSessionKey)) {
+				if (ticketResponse.decrypt(tgsSessionKey)) {
+					if(n2 == ticketResponse.getNonce()) {
 					serverTicket = ticketResponse.getResponseTicket();
 					serverSessionKey = ticketResponse.getSessionKey();
+					} else {
+						System.out.println("Ticket not verified");
+					}
 				}
 			}
-			//ServerTicket nun vorhanden? ANsonsten Ticket Response nicht entschlüsselbar etc...
-			if(serverTicket != null){
+			// ServerTicket nun vorhanden? ANsonsten Ticket Response nicht
+			// entschlüsselbar etc...
+			if (serverTicket != null) {
 				Auth serverAuth = new Auth(currentUser, (new Date()).getTime());
 				serverAuth.encrypt(serverSessionKey);
-				//Service anfordern
-				status = myFileserver.requestService(serverTicket, serverAuth, "showFile", filePath);
-				
+				// Service anfordern
+				status = myFileserver.requestService(serverTicket, serverAuth,
+						"showFile", filePath);
+
 			}
-			
-		
 		}
 		return status;
 	}
